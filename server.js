1baa5b47dd14887e24abdc859ffffa931922e377
Olivier Lüthy <olivier.luethy@kauz.ch>
2023-07-20 13:40:59 +0200

    Last push

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const server = http.createServer(app);
 // Create the WebSocket server
 const io = socketIO(server);
 
-// Map to store socket IDs and corresponding chat links
-const socketToChatLink = new Map();
+// Map to store socket IDs and corresponding usernames
+const socketToUsername = new Map();
 
 // Function to generate a unique chat link
 function generateChatLink() {
@@ -29,37 +29,39 @@ function generateChatLink() {
 }
 
 // WebSocket connection event
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   console.log("New WebSocket connection");
 
   // Event handling for 'join-room' event
-  socket.on("join-room", chatLink => {
-    console.log(`Joining room: ${chatLink}`);
+  socket.on("join-room", (username) => {
+    console.log(`Joining room: ${username}`);
 
-    // Store the socket ID and corresponding chat link
-    socketToChatLink.set(socket.id, chatLink);
+    // Store the socket ID and corresponding username
+    socketToUsername.set(socket.id, username);
 
-    // Join the chat room
-    socket.join(chatLink);
-
-    // Retrieve the chat history for the chat link
-    const chatHistory = chatMessages[chatLink] || [];
+    // Retrieve the chat history for the username
+    const chatHistory = chatMessages[username] || [];
     socket.emit("chat-history", chatHistory);
   });
 
   // Event handling for incoming messages
-  socket.on("message", data => {
+  socket.on("message", (data) => {
     console.log("New message:", data);
 
-    // Get the chat link associated with the socket
-    const chatLink = socketToChatLink.get(socket.id);
+    // Get the username associated with the socket
+    const username = socketToUsername.get(socket.id);
 
-    if (chatLink) {
-      // Store the message in the corresponding chat link's chatMessages (Replace with database storage in production)
-      chatMessages[chatLink].push(data.message);
+    if (username) {
+      // Store the message in the corresponding username's chatMessages (Replace with database storage in production)
+      chatMessages[username].push({
+        senderId: socket.id,
+        message: data.message,
+      });
 
-      // Broadcast the message to all sockets in the chat room
-      io.to(chatLink).emit("message", data);
+      // Broadcast the message to all sockets in the chat room except the sender
+      socket
+        .to(username)
+        .emit("message", { senderId: socket.id, message: data.message });
     }
   });
 
@@ -74,57 +76,42 @@ io.on("connection", socket => {
   socket.on("disconnect", () => {
     console.log("WebSocket connection disconnected");
 
-    // Remove the socket ID and corresponding chat link from the map
-    socketToChatLink.delete(socket.id);
+    // Remove the socket ID and corresponding username from the map
+    socketToUsername.delete(socket.id);
   });
 });
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// Generate a unique chat link
-app.get("/generate-chat-link", (req, res) => {
-  const chatLinkId = Math.floor(Math.random() * 1000000);
-  const chatLink = `/chat/${chatLinkId}`;
-
-  // Store chat link in memory (Replace with database storage in production)
-  chatMessages[chatLink] = [];
-
-  res.json({ chat_link: chatLink });
-});
-
-// Render chat HTML page for a chat link
-app.get("/chat/:chatLinkId", (req, res) => {
-  const chatLinkId = req.params.chatLinkId;
-  const chatLink = `/chat/${chatLinkId}`;
-
-  // Render the chat HTML page
-  res.sendFile(path.join(__dirname, "public", "chat.html"));
-});
-
-// Send a message to a chat link
-app.post("/send-message", bodyParser.json(), (req, res) => {
-  const chatLink = req.body.chat_link;
-  const message = req.body.message;
-
-  // Check if the chat link exists in the chatMessages object
-  if (chatMessages.hasOwnProperty(chatLink)) {
-    // Store message in the corresponding chat link's chatMessages (Replace with database storage in production)
-    chatMessages[chatLink].push(message);
-
-    // Broadcast the message to all connected sockets (tabs) in the specific chat room
-    io.to(chatLink).emit("message", { message });
-    
-    res.sendStatus(200);
+// Render chat HTML page for a username
+app.get("/chat", (req, res) => {
+  const username = req.query.username;
+  if (username) {
+    res.sendFile(path.join(__dirname, "public", "chat.html"));
   } else {
-    res.sendStatus(404); // Chat link not found
+    res.redirect("/");
   }
 });
 
+// Send a message to a username
+app.post("/send-message", bodyParser.json(), (req, res) => {
+  const username = req.body.username;
+  const message = req.body.message;
+
+  // Check if the username exists in the chatMessages object
+  if (chatMessages.hasOwnProperty(username)) {
+    // Store message in the corresponding username's chatMessages (Replace with database storage in production)
+    chatMessages[username].push(message);
+
+    // Broadcast the message to all connected sockets (tabs) in the specific chat room
+    io.to(username).emit("message", { message });
+
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404); // Username not found
+  }
+});
 
 // Start the server
 const port = process.env.PORT || 3000;
